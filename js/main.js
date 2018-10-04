a477a546762c59163e36f472183684ba12d06078
DrewShewaga <cierenius@gmail.com>
2018-10-04 03:44:42 -0400

    Mobile layout complete (with bugs)

--- js/main.js
+++ js/main.js
@@ -8,16 +8,21 @@
         navMenu = document.querySelector('.navMenu'),
         body = document.querySelector('body'),
         mWorks = document.querySelector('.worksMax'),
+        navDes = document.querySelector('.navDes'),
         dWorks = document.querySelector('.worksDrew'),
-        mbox = document.querySelector('.maxBox');
+        navDev = document.querySelector('.navDev'),
+        mbox = document.querySelector('.maxBox'),
+        dbox = document.querySelector('.drewBox'),
+        navHome = document.querySelector('.navHome');
 
 
 
     //FLICKITY
-    var elem = document.querySelector('.main-carousel');
+    var max = document.querySelector('.max-carousel');
+    var drew = document.querySelector('.drew-carousel');
     // var Flickity = require('flickity');
     // require('flickity-imagesloaded');
-    var flkty = new Flickity( elem, {
+    var flkty = new Flickity( max, {
       // options
       cellAlign: 'center',
       wrapAround: true,
@@ -25,6 +30,14 @@
       autoPlay: 8000,
       imagesLoaded: true
     });
+    var flkty = new Flickity( drew, {
+        // options
+        cellAlign: 'center',
+        wrapAround: true,
+        initialIndex: 1,
+        autoPlay: 8000,
+        imagesLoaded: true
+      });
   
 
     //FUNCTIONS
@@ -35,13 +48,38 @@
         }
         else {
             navMenu.classList.add('hidden');
+
+            if (mbox.classList.value == 'maxBox hidden'){
             body.classList.remove('scrollStop');
+            }
+            else {
+
+            }
         }
+    }
+
+    function closeMenu() {
+        navMenu.classList.add('hidden');
     }
 
 
     function showMax() {
         mbox.classList.remove('hidden');
+        body.classList.add('scrollStop');
+        dbox.classList.add('hidden');
+    }
+
+    function showDrew() {
+        dbox.classList.remove('hidden');
+        body.classList.add('scrollStop');
+        mbox.classList.add('hidden');
+    }
+
+    function closeLB() {
+        mbox.classList.add('hidden');
+        dbox.classList.add('hidden');
+        body.classList.remove('scrollStop');
+        navMenu.classList.add('hidden');
     }
 
 
@@ -49,4 +87,11 @@
     //EVENT HANDLING
     navIcon.addEventListener('click', showMenu);
     mWorks.addEventListener('click', showMax);
+    navDes.addEventListener('click', showMax);
+    dWorks.addEventListener('click', showDrew);
+    navDev.addEventListener('click', showDrew);
+    navDes.addEventListener('click', closeMenu);
+    navDev.addEventListener('click', closeMenu);
+    navHome.addEventListener('click', closeLB);
+    exit = document.querySelectorAll('.exit').forEach(exit => exit.addEventListener('click', closeLB));
 })();
